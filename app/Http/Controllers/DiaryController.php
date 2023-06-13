c70c60bc7fce50f681514eb567ddae4edfd9b0d2
zakitono <coppercraft1313@gmail.com>
2023-06-13 22:09:34 +0900

    変更画面用のメソッドをコントローラーに追記

--- app/Http/Controllers/DiaryController.php
+++ app/Http/Controllers/DiaryController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\Diary;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class DiaryController extends Controller
 {
@@ -20,5 +21,31 @@ class DiaryController extends Controller
         $diary = Diary::find($id);
 
         return view('diary.detail', ['diary' => $diary]);
+    }
+
+    public function edit($id)
+    {
+        $diary = Diary::find($id);
+
+        return view('diary.edit', ['diary' => $diary]);
+    }
+
+    public function update(Request $request)
+    {
+        try {
+            DB::beginTransaction();
+            $diary = Diary::find($request->input('id'));
+            $diary->date = $request->input('date');
+            $diary->note = $request->input('note');
+            $diary->note01 = $request->input('note01');
+            $diary->note02 = $request->input('note02');
+            $diary->save();
+            DB::commit();
+            return redirect('diary')->with('status', '内容を更新しました。');
+        } catch (\Exception $ex) {
+            DB::rollback();
+            logger($ex->getMessage());
+            return redirect('diary')->withErrors($ex->getMessage());
+        }
     }
 }
