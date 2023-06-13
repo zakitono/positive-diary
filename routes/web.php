cd59a3d4560824d9a28d42c1552483de03a4ad3e
zakitono <coppercraft1313@gmail.com>
2023-06-13 22:07:55 +0900

    変更画面用のルーティングを追加

--- routes/web.php
+++ routes/web.php
@@ -32,6 +32,9 @@ Route::middleware('auth')->group(function () {
 Route::middleware(['auth'])->prefix('diary')->group(function () {
     Route::get('/', [DiaryController::class, 'index'])->name('diary');
     Route::get('/detail/{id}', [DiaryController::class, 'detail'])->name('diary.detail');
+    Route::get('/edit/{id}', [DiaryController::class, 'edit'])->name('diary.edit');
+
+    Route::patch('/update', [DiaryController::class, 'update'])->name('diary.update');
 });
 
 require __DIR__ . '/auth.php';
